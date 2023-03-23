842accfe5d395f793d7725b2917c05fe90f6cbd0
Soh Wen Cong <sohbryon@gmail.com>
2023-03-23 16:54:43 +0800

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -5,11 +5,10 @@ def error_message_detail(error,error_detail:sys):  # error message function
     _,_,exc_tb=error_detail.exe.info()
     file_name=exc_tb.tb_frame.f_code.co_filename  # assign variable
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
+        file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
-    )
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
