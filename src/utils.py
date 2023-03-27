9df1bb3b86477f904969349c2afb380b7bc9bd23
Soh Wen Cong <sohbryon@gmail.com>
2023-03-27 18:42:08 +0800

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill  # for pickle file
-# from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score
 # from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
@@ -18,6 +18,37 @@ def save_object(file_path, obj):  # to save pickle file
 
         with open(file_path, "wb") as file_obj:  # open file in wb format (binary write)
             dill.dump(obj, file_obj)  # obj - dumped # file_obj - to be written
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}  # empty dictionary
+
+        for i in range(len(list(models))):  # loop through every model
+            model = list(models.values())[i]
+            # para=param[list(models.keys())[i]]
+
+            # gs = GridSearchCV(model,para,cv=3)
+            # gs.fit(X_train,y_train)
+
+            # model.set_params(**gs.best_params_)
+            # model.fit(X_train,y_train)
+
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)  # predict
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)  # r2 score
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score  # append with every loop
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
